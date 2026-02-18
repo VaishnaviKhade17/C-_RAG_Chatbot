c330d6a496ef3c68317ccc950dd31c04100339f7
VaishnaviKhade17 <vaishnavikhade521@gmail.com>
2026-02-18 15:26:37 +0530

    Add files via upload

---
+++ chatBotDemo.py
@@ -0,0 +1,54 @@
+import streamlit as st
+
+# Import LangChain
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+
+# Page config
+st.set_page_config(page_title="C++ RAG Chatbot")
+st.title("C++ RAG Chatbot")
+st.write("Ask any question related to C++ Introduction")
+
+# Load and cache vectorstore
+@st.cache_resource
+def load_vectorstore():
+
+    # Load document
+    loader = TextLoader("C++_introduction.txt", encoding="utf-8")
+    documents = loader.load()
+
+    # Split text
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=200,
+        chunk_overlap=20
+    )
+    final_documents = text_splitter.split_documents(documents)
+
+    # Embeddings
+    embeddings = HuggingFaceEmbeddings(
+        model_name="all-MiniLM-L6-v2"
+    )
+
+    # Create FAISS vector store
+    db = FAISS.from_documents(final_documents, embeddings)
+
+    return db
+
+
+# Load DB
+db = load_vectorstore()
+# -------------------------
+# User Input
+# -------------------------
+
+query = st.text_input("Enter your question about C++:")
+
+if query:
+    docs = db.similarity_search(query, k=3)
+    st.subheader("Retrieved Context:")
+
+    for i, doc in enumerate(docs):
+        st.markdown(f"**Result {i+1}:**")
+        st.write(doc.page_content)
